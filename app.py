5a3f4e7b2c0bf97435c4908b73eed32e672e09d1
awajanhc <awajanhc@gmail.com>
2026-01-14 09:35:57 +0300

    Update links and add new resources for 2026
    Updated links for 2026 and added new links for performance, contracts, and documents.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# --- CONFIGURATION: UPDATE YOUR LINKS HERE ---
-# If a link changes next year, just swap it here.
+# --- UPDATED LINKS 2026 ---
 LINKS = {
     "timesheet": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7B14B58B1C-7889-482D-B15C-FD10A92E11F4%7D&file=Health%20DP%20staff%20Timesheet.xlsx&fromShare=true&action=default&mobileredirect=true",
     "requests": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7B8F51B034-6A3E-479E-B72A-2BD2A24E75E1%7D&file=REQUEST%20FORM_JFO_Temp%20Replacement.xlsx&fromShare=true&action=default&mobileredirect=true",
@@ -11,15 +11,20 @@ LINKS = {
     "replacement_data": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7BF8D49114-2D59-45F1-BFC4-28131329E5FE%7D&file=بيانات%20الموظفين%20البدلاء.xlsx&fromShare=true&action=default&mobileredirect=true",
     "load_time": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7BE3DE7CA0-279B-48C8-8887-23DC3AE346AF%7D&file=Copy%20of%20Load%20Work%20Time%20for%20Zarka%20DRs%20-%202022.xlsx&wdLOR=c1D546678-86BC-42C8-8810-4059AC58C5FE&fromShare=true&action=default&mobileredirect=true",
     "encd": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7B36B826FC-7710-4FE2-AA08-D5B8520F50DD%7D&file=Copy%20of%20Copy%20of%20NCD%20application-%20Zarka%20Area%20-%20Copy.xlsx&wdLOR=cA8C5456A-DD17-4DD2-9F9E-370521C97FDF&fromShare=true&action=default&mobileredirect=true",
-    "emch": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alicashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7B0EB8E0DC-A41C-4880-A966-625DDFE550B4%7D&file=Copy%20of%20Copy%20of%20MCH%20%20application-%20Zarka%20Area.xlsx&wdLOR=c9B8AD8B6-CEB6-428B-B793-D0561D4DA777&fromShare=true&action=default&mobileredirect=true",
+    "emch": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7B0EB8E0DC-A41C-4880-A966-625DDFE550B4%7D&file=Copy%20of%20Copy%20of%20MCH%20%20application-%20Zarka%20Area.xlsx&wdLOR=c9B8AD8B6-CEB6-428B-B793-D0561D4DA777&fromShare=true&action=default&mobileredirect=true",
     "manning": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7B1348D2C9-BB99-4CD0-8F41-48788004CDC2%7D&file=Manning%20Table%20%20%20%20Health%20%20%20Zarka%20%20%2019.03.2025.xlsx&wdLOR=cEE240FC3-1BFE-4864-9D80-F863855BDE30&fromShare=true&action=default&mobileredirect=true",
-    "daily_paid": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7BA8136684-5D00-41EF-A450-C9F8627DB25F%7D&file=All%20DP%20-%20Health%20-%20Zarka_.xlsx&wdLOR=c17AB2563-A195-4392-8180-9005FA4001E2&fromShare=true&action=default&mobileredirect=true"
+    "daily_paid": "https://unrwaorg-my.sharepoint.com/:x:/r/personal/n_alnashash_unrwa_org/_layouts/15/Doc.aspx?sourcedoc=%7BA8136684-5D00-41EF-A450-C9F8627DB25F%7D&file=All%20DP%20-%20Health%20-%20Zarka_.xlsx&wdLOR=c17AB2563-A195-4392-8180-9005FA4001E2&fromShare=true&action=default&mobileredirect=true",
+    
+    # NEW LINKS ADDED BELOW:
+    "performance_2026": "https://unrwaorg.sharepoint.com/:f:/r/sites/ZarkaArea/Shared%20Documents/%D8%AA%D9%82%D9%8A%D9%8A%D9%85%20%D8%A3%D8%AF%D8%A7%D8%A1%20%D8%A7%D9%84%D9%85%D9%88%D8%B8%D9%81%D9%8A%D9%86%20%D8%A7%D9%84%D8%A8%D8%AF%D9%84%D8%A7%D8%A1%202026?csf=1&web=1&e=r4F0RK",
+    "contracts": "https://unrwaorg.sharepoint.com/:f:/r/sites/ZarkaArea/Shared%20Documents/%D8%B9%D9%82%D9%82%D9%88%D8%AF%20%D8%A7%D9%84%D9%85%D9%88%D8%B8%D9%81%D9%8A%D9%86%20%D8%A7%D9%84%D8%A8%D8%AF%D9%84%D8%A7%D8%A1?csf=1&web=1&e=0yvfgj",
+    "documents": "https://unrwaorg.sharepoint.com/:f:/r/sites/ZarkaArea/Shared%20Documents/%D9%88%D8%AB%D8%A7%D8%A6%D9%82%20%D8%A7%D9%84%D9%85%D9%88%D8%B8%D9%81%D9%8A%D9%86%20%D8%A7%D9%84%D8%A8%D8%AF%D9%84%D8%A7%D8%A1?csf=1&web=1&e=bsj8nX"
 }
 
 @app.route('/')
 def home():
-    # We pass the 'LINKS' dictionary to the HTML template
     return render_template('index.html', links=LINKS)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
